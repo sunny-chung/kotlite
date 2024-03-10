4ea652c9b022e38d71f1fd5fa09915f8b169e1b1
Sunny Chung <sunnychung@live.hk>
2024-03-10 16:54:34 +0800

    add test for `interface` extension functions

--- interpreter/src/commonTest/kotlin/com/sunnychung/lib/multiplatform/kotlite/test/interpreter/InterfaceTest.kt
+++ interpreter/src/commonTest/kotlin/com/sunnychung/lib/multiplatform/kotlite/test/interpreter/InterfaceTest.kt
@@ -34,4 +34,59 @@ class InterfaceTest {
         assertEquals(24, (symbolTable.findPropertyByDeclaredName("a") as IntValue).value)
         assertEquals(36, (symbolTable.findPropertyByDeclaredName("b") as IntValue).value)
     }
+
+    @Test
+    fun extensionFunction1() {
+        val interpreter = interpreter("""
+            interface I {
+                fun f(x: Int): Int
+            }
+            class A : I {
+                override fun f(x: Int) = 2 * x
+            }
+            class B : I {
+                override fun f(x: Int) = 3 * x
+            }
+            fun I.g(x: Int) = f(x) + 1 
+            fun getInstance(x: Int): I {
+                return if (x > 0) {
+                    A()
+                } else {
+                    B()
+                }
+            }
+            val a = getInstance(1).g(12)
+            val b = getInstance(-1).g(12)
+        """.trimIndent())
+        interpreter.eval()
+        val symbolTable = interpreter.callStack.currentSymbolTable()
+        println(symbolTable.propertyValues)
+        assertEquals(2, symbolTable.propertyValues.size)
+        assertEquals(25, (symbolTable.findPropertyByDeclaredName("a") as IntValue).value)
+        assertEquals(37, (symbolTable.findPropertyByDeclaredName("b") as IntValue).value)
+    }
+
+    @Test
+    fun extensionFunction2() {
+        val interpreter = interpreter("""
+            interface I {
+                fun f(x: Int): Int
+            }
+            class A : I {
+                override fun f(x: Int) = 2 * x
+            }
+            class B : I {
+                override fun f(x: Int) = 3 * x
+            }
+            fun I.g(x: Int) = f(x) + 1
+            val a = A().g(12)
+            val b = B().g(12)
+        """.trimIndent())
+        interpreter.eval()
+        val symbolTable = interpreter.callStack.currentSymbolTable()
+        println(symbolTable.propertyValues)
+        assertEquals(2, symbolTable.propertyValues.size)
+        assertEquals(25, (symbolTable.findPropertyByDeclaredName("a") as IntValue).value)
+        assertEquals(37, (symbolTable.findPropertyByDeclaredName("b") as IntValue).value)
+    }
 }
