48ccc1046ab1c67fef987a5c437bff3fc420d6f7
Sunny Chung <sunnychung@live.hk>
2024-04-06 17:17:51 +0800

    add documentation

---
+++ doc/generate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Please execute this script inside the `/doc` directory
+
+rm -Rf output
+docker run --rm -it -v "$(pwd)/..:/wd" uwebarthel/asciidoctor asciidoctor \
+  -r asciidoctor-diagram \
+  -D "/wd/doc/output" \
+  /wd/doc/usermanual/index.adoc
