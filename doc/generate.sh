51f9b5b19a7d557cffd1c605c01e387cd44c0bce
Sunny Chung <sunnychung@live.hk>
2024-04-06 23:19:54 +0800

    update documentation

--- doc/generate.sh
+++ doc/generate.sh
@@ -7,3 +7,4 @@ docker run --rm -it -v "$(pwd)/..:/wd" uwebarthel/asciidoctor asciidoctor \
   -r asciidoctor-diagram \
   -D "/wd/doc/output" \
   /wd/doc/usermanual/index.adoc
+cp -R usermanual/img output/img
