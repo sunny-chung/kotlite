268d1cd2e0723422e96e27c2d504c6ea119fe895
Sunny Chung <sunnychung@live.hk>
2024-04-16 22:26:45 +0800

    fix SourcePosition JVM constructors to make it compatible with v1.0.0

--- interpreter/src/commonMain/kotlin/com/sunnychung/lib/multiplatform/kotlite/model/SourcePosition.kt
+++ interpreter/src/commonMain/kotlin/com/sunnychung/lib/multiplatform/kotlite/model/SourcePosition.kt
@@ -2,11 +2,14 @@ package com.sunnychung.lib.multiplatform.kotlite.model
 
 import com.sunnychung.lib.multiplatform.kotlite.lexer.BuiltinFilename
 
-data class SourcePosition(val filename: String, val lineNum: Int, val col: Int, val index: Int = 0) {
+data class SourcePosition(val filename: String, val lineNum: Int, val col: Int, val index: Int) {
     companion object {
         val NONE = SourcePosition("", 1, 1)
         val BUILTIN = SourcePosition(BuiltinFilename.BUILTIN, 1, 1)
     }
+
+    // make it compatible with interpreter:1.0.0-jvm
+    constructor(filename: String, lineNum: Int, col: Int) : this(filename, lineNum, col, 0)
 
     override fun toString(): String {
         return "[$filename:$lineNum:$col]"
